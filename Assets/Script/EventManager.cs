24f4fda44d899e7b073fe5fd63e940265904f227
Moskate <47504837+Moskate@users.noreply.github.com>
2020-04-17 16:29:30 -0700

    Golden key update
    collecting the three keys will now make the golden key appear. The golden key doesn't do anything yet

--- Assets/Script/EventManager.cs
+++ Assets/Script/EventManager.cs
@@ -9,7 +9,8 @@ public class EventManager : MonoBehaviour
     public int buttonHeight;
     private int origin_x;
     private int origin_y;
-    public UnityEngine.Events.UnityEvent DoorAppears;
+    public UnityEngine.Events.UnityEvent GoldenKey;
+    bool goldkeymade;
     GUIStyle style;
     // Start is called before the first frame update
     void Start()
@@ -20,6 +21,7 @@ public class EventManager : MonoBehaviour
         origin_x = 100;
         origin_y = 20;
         style = new GUIStyle();
+        goldkeymade = false;
     }
     private void OnGUI()
     {
@@ -29,6 +31,10 @@ public class EventManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(counter == 3 && !goldkeymade)
+        {
+            GoldenKey.Invoke();
+            goldkeymade = true;
+        }
     }
 }
---
+++ Assets/Script/goldenKey.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class goldenKey : MonoBehaviour
+{
+    public GameObject Goldenkey;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void GoldenKeyMake()
+    {
+        Instantiate(Goldenkey, transform.position, Quaternion.identity);
+    }
+}
