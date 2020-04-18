29653493cdca44a3b3d68ecdd2a60bf1b3a05366
Kwin094 <62923495+Kwin094@users.noreply.github.com>
2020-04-17 21:37:59 -0700

    added enemy, doesn't work

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+	public Transform target;
+	private float speed;
+	public GameObject explosion;
+    private AudioSource audio1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 4f;
+        audio1 = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target);
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+
+    /*
+    void OnCollisionEnter(Collision col) {
+    	if(col.gameObject.tag == "Bullet") {
+            audio1.Play();
+            Destroy(col.gameObject);
+    		Instantiate(explosion, transform.position, transform.rotation);
+    		Destroy(this);
+    		Destroy(gameObject);
+    	}
+    }*/
+}
+
+
+
+
+
+
+
