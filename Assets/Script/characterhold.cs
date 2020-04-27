9fea872520f92b108cbe6b79ba88795d4a29125b
Moskate <47504837+Moskate@users.noreply.github.com>
2020-04-27 15:57:54 -0700

    new moving platforms
    added two new platforms that move. Can be duplicated as needed. Need to find way to desync them so the move in different order

---
+++ Assets/Script/characterhold.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterhold : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        other.transform.parent = gameObject.transform;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        other.transform.parent = null;
+    }
+}
---
+++ Assets/Script/platformMove.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platformMove : MonoBehaviour
+{
+    public Transform movingPlat;
+    public Transform post1;
+    public Transform post2;
+    public Vector3 newPost;
+    public string currentstate;
+    public float smooth;
+    public float reset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TargetChange();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        movingPlat.position = Vector3.Lerp(movingPlat.position, newPost, smooth * Time.deltaTime);
+    }
+    void TargetChange()
+    {
+        if (currentstate == "post1")
+        {
+            currentstate = "post2";
+            newPost = post2.position;
+        }
+        else if(currentstate == "post2")
+        {
+            currentstate = "post1";
+            newPost = post1.position;
+        }
+        else if(currentstate == "")
+        {
+            currentstate = "post2";
+            newPost = post2.position;
+        }
+        Invoke("TargetChange", reset);
+    }
+        
+    
+}
